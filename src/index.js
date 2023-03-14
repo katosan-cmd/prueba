525befe494a85dde3b288c01d65f07a9a31e6993
katosan <jonisai3@gmail.com>
2023-03-14 01:21:30 -0600

    segundo commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const fn = () => {
+    const miString = "soy un string!"
+    let b = miString + " soy feliz"
+    console.log(b)
+                                                                                                            
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "pajarito feliz"
+    
+    console.log(a)
+}
